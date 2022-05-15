156eac4d5e4854b4207687ce16c8156772086c1b
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-15 13:03:07 +0300

    Feat :  Adds the user model

---
+++ app/models.py
@@ -0,0 +1,38 @@
+from . import db, login_manager
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(100))
+    bio = db.Column(db.String(255))
+    email = db.Column(db.String(100),nullable = False,unique = True)
+    avatar = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    password_secure = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.password_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_secure,password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
