0a6999acc6455866a7a330c3db728d63dc3b2e78
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-17 11:17:18 +0300

    Deployment to Heroku

--- config.py
+++ config.py
@@ -50,7 +50,6 @@ class TestConfig(Config):
     '''
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
-DEBUG = True
 
 
 class DevConfig(Config):
@@ -61,7 +60,6 @@ class DevConfig(Config):
     '''
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
-DEBUG = True
 
 config_options = {
 'development':DevConfig,
